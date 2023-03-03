f4b96213fbbaa67a57828c00d5a9c1de1730a659
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-03-03 23:22:56 +0300

    RabbitMQ Consumer Project Created

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+
+// Connection Created
+ConnectionFactory factory = new();
+
+factory.Uri = new("amqps://tfnsxluf:Q7118mxTG5u1eYr4qIUyUtwmG2C8PAm5@woodpecker.rmq.cloudamqp.com/tfnsxluf");
+
+
+// Connection activeted and channel opened
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+//Queuo Created
+
+channel.QueueDeclare(queue: "example-queue", exclusive:false);
+
+
+//Queue message send 
+
+//RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir.. 
+
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+Console.Read();
