1251a3efd99a47e3fbdcee8adfbced054fbe4c2f
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-03-04 18:18:26 +0300

    Direct Exchange completed

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -1,4 +1,4 @@
-﻿//Connection createsd
+﻿//Consumer 
 
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
@@ -10,27 +10,30 @@ ConnectionFactory factory = new();
 factory.Uri = new("amqps://tfnsxluf:Q7118mxTG5u1eYr4qIUyUtwmG2C8PAm5@woodpecker.rmq.cloudamqp.com/tfnsxluf");
 
 
-// Connection activeted and channel opened
 
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
+//1
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+//2
+string queueName =channel.QueueDeclare().QueueName;
+//3
+channel.QueueBind(queue: queueName, exchange: "direct-exchange-example",
+    routingKey: "direct-queue-example");
 
 
-//Queue Created
-
-channel.QueueDeclare(queue: "example-queue", exclusive: false, durable:false); // Consumer Queue  == Publicher Queue
 EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: queueName, autoAck: true, consumer:consumer);
 
-//autoAck mesaj kuruktan alındıktan sonra silinip silinemesi
-channel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
-channel.BasicQos(0, 1, false);
-
-//kuyruğa gelen mesajın işlendiği yer 
 consumer.Received += (sender, e) =>
 {
-    //e.Body :kuyruktaki mesajın verisini bütünsel olarak getirecektir.
-    //e.Body.Span veya e.Body.toArray() : Kuyruktaki mesajın byte dönüştürmemiz gerekmektedir.
-    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
-    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message); ;
 };
 
+Console.Read();
+
+
+//1 Publisher daki aynı kuyruk ve type a sahip olması gerekir 
+
+//2. adım:  publisher tarafından routing key'de bulunan değerdeki kuyruğa gönderileb mesajları endi oluşturduğumuz kuyruğa yönlendirmemiz gerekir. Bunun için önceliikler bir kuyruk oluşturmamız gerekmektedir.
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,40 +1,28 @@
-﻿using RabbitMQ.Client;
+﻿//Publisher 
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
-
-
-// Connection Created
 ConnectionFactory factory = new();
+
 
 factory.Uri = new("amqps://tfnsxluf:Q7118mxTG5u1eYr4qIUyUtwmG2C8PAm5@woodpecker.rmq.cloudamqp.com/tfnsxluf");
 
 
-// Connection activeted and channel opened
 
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-
-//Queuo Created
-
-channel.QueueDeclare(queue: "example-queue", exclusive:false, durable:true);
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
 
 
-//Queue message send 
-
-//RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir.. 
-
-//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
-//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
-//Console.Read();
-
-IBasicProperties properties = channel.CreateBasicProperties();
-properties.Persistent = true;
-
-for (int i = 0; i < 100; i++)
+while (true)
 {
-    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
-    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties:properties);
-    Console.Read();
+    Console.Write("Mesaj . ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "direct-exchange-example", routingKey:"direct-queue-example", body: byteMessage);
+}    
 
-}
+Console.Read();
