c6adc15d4ce26a3af3100f23e84d3bfadda02e2d
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-03-05 17:02:40 +0300

    Topic Excahnge completed

---
+++ TopicExchange.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿//Consumer 
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+
+factory.Uri = new("amqps://tfnsxluf:Q7118mxTG5u1eYr4qIUyUtwmG2C8PAm5@woodpecker.rmq.cloudamqp.com/tfnsxluf");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "topic-exchange-example",
+    type: ExchangeType.Topic);
+
+Console.WriteLine("Dinlenecek topic formatını belkirtliniz: ");
+string topic = Console.ReadLine();
+string queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(
+    queue: queueName,
+    exchange: "topic-exchange-example",
+    routingKey: topic
+    );
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ TopicExchange.Publisher/Program.cs
@@ -0,0 +1,34 @@
+﻿//Publisher 
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+
+factory.Uri = new("amqps://tfnsxluf:Q7118mxTG5u1eYr4qIUyUtwmG2C8PAm5@woodpecker.rmq.cloudamqp.com/tfnsxluf");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange:"topic-exchange-example",
+    type: ExchangeType.Topic);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i} ");
+    string topic = Console.ReadLine();
+    channel.BasicPublish(exchange: "topic-exchange-example",
+        routingKey: topic,
+        body: message);
+}
+
+
+
+
+Console.Read();
