d1e1bef9f14afbbf84ddb32026159654b83b36b2
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-03-09 23:24:40 +0300

    P2P (Point-To-Point) Design applied

---
+++ RabbitMQ.MessageTemplates.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿//Consumer 
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+
+factory.Uri = new("amqps://tfnsxluf:Q7118mxTG5u1eYr4qIUyUtwmG2C8PAm5@woodpecker.rmq.cloudamqp.com/tfnsxluf");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+#region P'P (Point-To-Point) Design
+string queueName = "exampke-p2p-queue";
+
+channel.QueueDeclare(queue: queueName,
+    durable:false,
+    exclusive:false,
+    autoDelete:false);
+
+byte[] message = Encoding.UTF8.GetBytes("Hello");
+channel.BasicPublish(exchange: string.Empty,
+    routingKey: queueName,
+    body: message);
+#endregion
+
+
+
+Console.Read();
+
+
---
+++ RabbitMQ.MessageTemplates.Publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿//Publisher 
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+
+factory.Uri = new("amqps://tfnsxluf:Q7118mxTG5u1eYr4qIUyUtwmG2C8PAm5@woodpecker.rmq.cloudamqp.com/tfnsxluf");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+#region P'P (Point-To-Point) Design
+string queueName = "exampke-p2p-queue";
+
+channel.QueueDeclare(queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue:queueName ,
+    autoAck:false,
+    consumer: consumer);
+
+consumer.Received += (sernder, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+}; 
+#endregion
+
+
+
+
+Console.Read();
