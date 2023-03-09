0fe5a6e01418d1e5003499fe8bc3915f7ac9f570
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-03-09 23:09:59 +0300

    .

--- HeaderExchange.Consumer/Program.cs
+++ HeaderExchange.Consumer/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Consumer 
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+
+factory.Uri = new("amqps://tfnsxluf:Q7118mxTG5u1eYr4qIUyUtwmG2C8PAm5@woodpecker.rmq.cloudamqp.com/tfnsxluf");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers
+    );
+
+Console.Write("Lütfen header value'sunu giriniz");
+string value = Console.ReadLine();
+
+string queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(
+    queue: queueName,
+    exchange: "header-exchange-example",
+    routingKey: string.Empty,
+    new Dictionary<string, object>
+    {
+        ["no"]= value
+    });
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+
+
+
+
+Console.Read();
--- HeaderExchange.Publisher/Program.cs
+++ HeaderExchange.Publisher/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Publisher 
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+
+factory.Uri = new("amqps://tfnsxluf:Q7118mxTG5u1eYr4qIUyUtwmG2C8PAm5@woodpecker.rmq.cloudamqp.com/tfnsxluf");
+
+
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(
+    exchange: "header-exchange-example",
+    type: ExchangeType.Headers
+    );
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+    Console.Write("Litfen header value'sunu giriniz: ");
+    string value = Console.ReadLine();
+
+    IBasicProperties basicProperties = channel.CreateBasicProperties();
+    basicProperties.Headers = new Dictionary<string, object>
+    {
+        ["no"] = value
+    };
+
+    channel.BasicPublish(
+        "header-exchange-examplke",
+    routingKey: string.Empty,
+    body: message,
+    basicProperties: basicProperties
+        );
+}
+
+
+Console.Read();
