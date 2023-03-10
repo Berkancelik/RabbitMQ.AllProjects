51f2c4d5e9a03f392c60238ada0b6038e1793203
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-03-10 22:28:47 +0300

    Request/Response Deisgn​ applied

--- RabbitMQ.MessageTemplates.Publisher/Program.cs
+++ RabbitMQ.MessageTemplates.Publisher/Program.cs
@@ -56,29 +56,71 @@ using IModel channel = connection.CreateModel();
 
 #endregion
 
-#region (Workı Queue Design)
+#region (Work Queue Design)
 
-string queueName = "example-work-queue";
+//string queueName = "example-work-queue";
 
-channel.QueueDeclare(queue: queueName,
-    durable: false, exclusive: false, autoDelete: false);
- 
-for (int i = 0; i < 100; i++)
-{
-    await Task.Delay(1000);
+//channel.QueueDeclare(queue: queueName,
+//    durable: false, exclusive: false, autoDelete: false);
 
-    byte[] message = Encoding.UTF8.GetBytes("merhaba" + i);
+//for (int i = 0; i < 100; i++)
+//{
+//    await Task.Delay(1000);
 
-    channel.BasicPublish(
-        exchange: queueName,
-        routingKey: string.Empty,
-        body: message);
-}
+//    byte[] message = Encoding.UTF8.GetBytes("merhaba" + i);
+
+//    channel.BasicPublish(
+//        exchange: queueName,
+//        routingKey: string.Empty,
+//        body: message);
+//}
 
 
 #endregion
 
+#region Request/Response Tasarımı​
+string requestQueueName = "example-request-response-queue";
+channel.QueueDeclare(
+    queue: requestQueueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
 
+string replyQueueName = channel.QueueDeclare().QueueName;
 
+string correlationId = Guid.NewGuid().ToString();
+
+#region Request Mesajını Oluşturma ve Gönderme
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.CorrelationId = correlationId;
+properties.ReplyTo = replyQueueName;
+
+for (int i = 0; i < 10; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("merhaba" + i);
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: requestQueueName,
+        body: message,
+        basicProperties: properties);
+}
+#endregion
+#region Response Kuyruğu Dinleme
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: replyQueueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    if (e.BasicProperties.CorrelationId == correlationId)
+    {
+        //....
+        Console.WriteLine($"Response : {Encoding.UTF8.GetString(e.Body.Span)}");
+    }
+};
+#endregion
+#endregion
 
 Console.Read();
