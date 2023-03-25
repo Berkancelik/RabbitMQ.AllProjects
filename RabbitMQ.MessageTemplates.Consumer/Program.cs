01bdac510685d0281aac419f0721f47d787cdfb0
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-03-25 15:23:25 +0300

    ESB Infrastructure projects added

--- RabbitMQ.MessageTemplates.Consumer/Program.cs
+++ RabbitMQ.MessageTemplates.Consumer/Program.cs
@@ -15,21 +15,103 @@ using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
 #region P2P (Point-To-Point) Design
-string queueName = "exampke-p2p-queue";
+//string queueName = "example-p2p-queue";
 
-channel.QueueDeclare(queue: queueName,
-    durable:false,
-    exclusive:false,
-    autoDelete:false);
+//channel.QueueDeclare(queue: queueName,
+//    durable:false,
+//    exclusive:false,
+//    autoDelete:false);
 
-byte[] message = Encoding.UTF8.GetBytes("Hello");
-channel.BasicPublish(exchange: string.Empty,
-    routingKey: queueName,
-    body: message);
+//byte[] message = Encoding.UTF8.GetBytes("Hello");
+//channel.BasicPublish(exchange: string.Empty,
+//    routingKey: queueName,
+//    body: message);
+#endregion
+
+#region (Publis-Subscribe) Design
+
+//string exchangeName = "example-pub-sub-exchange";
+
+//channel.ExchangeDeclare(
+//    exchange: exchangeName,
+//    type: ExchangeType.Fanout);
+
+//string queueName = channel.QueueDeclare().QueueName;
+//channel.QueueBind(
+//    queue: queueName,
+//    exchange: queueName,
+//    routingKey: string.Empty);
+
+//EventingBasicConsumer consumer = new (channel);
+//channel.BasicConsume(
+//    queue: queueName,
+//    autoAck: false,
+//    consumer: consumer);
+
+//consumer.Received += (sender, e) =>
+//{
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//};
+
+#endregion
+
+#region (Work Queue Design)
+
+//string queueName = "example-work-queue";
+
+//channel.QueueDeclare(queue: queueName,
+//    durable: false, exclusive: false, autoDelete: false);
+
+//EventingBasicConsumer consumer = new (channel);
+
+//channel.BasicConsume(queue:queueName,
+//    autoAck:true,
+//    consumer:consumer);
+
+//channel.BasicQos(
+//    prefetchCount: 1,
+//    prefetchSize: 0,
+//    global: false);
+
+//consumer.Received += (sender, e) =>
+//{
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//};
+
 #endregion
 
 
+#region Request/Response Deisgn​
 
+string requestQueueName = "example-request-response-queue";
+channel.QueueDeclare(
+    queue: requestQueueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: requestQueueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+    //.....
+    byte[] responseMessage = Encoding.UTF8.GetBytes($"İşlem tamamlandı. : {message}");
+    IBasicProperties properties = channel.CreateBasicProperties();
+    properties.CorrelationId = e.BasicProperties.CorrelationId;
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: e.BasicProperties.ReplyTo,
+        basicProperties: properties,
+        body: responseMessage);
+};
+
+#endregion
 
 
 Console.Read();
