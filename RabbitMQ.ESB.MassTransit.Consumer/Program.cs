66fefc62fa8837287b50bbd3f2c1ba5f098e06dc
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-03-25 19:00:56 +0300

    Enterpriser Service Bus completed

--- RabbitMQ.ESB.MassTransit.Consumer/Program.cs
+++ RabbitMQ.ESB.MassTransit.Consumer/Program.cs
@@ -1,9 +1,23 @@
 ﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.Consumer.Consumers;
 
 string rabbitMQUri = "amqps://tfnsxluf:Q7118mxTG5u1eYr4qIUyUtwmG2C8PAm5@woodpecker.rmq.cloudamqp.com/tfnsxluf";
 
 string queueName = "example-queue";
 
-//IBusControl bus = Bus.Factory.CreateUsingRabbitMQ
+
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+    factory.ReceiveEndpoint(queueName, endpoint =>
+    {
+        endpoint.Consumer<ExampleMessageConsumer>();
+    });
+});
+
+bus.StartAsync();
+
+Console.Read();
 
 
